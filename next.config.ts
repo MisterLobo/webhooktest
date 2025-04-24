7633e179052242da201c969ff63cbee1455c5232
MisterLobo <algae1234567890@gmail.com>
2025-04-24 13:56:43 +0800

    allow remote urls for images

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'github.com',
+        protocol: 'https',
+        pathname: '/*',
+      }
+    ],
+  },
+  experimental: {
+    nextScriptWorkers: true,
+  },
 };
 
 export default nextConfig;
